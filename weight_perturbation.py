bfb085c177135fc89473150f9bd66fd6adfe8f36
M.Kraus <mkkraus@email.cz>
2024-11-30 13:15:27 +0100

    Working SNN, needs testing on real task!

--- weight_perturbation.py
+++ weight_perturbation.py
@@ -1,4 +1,4 @@
-def update_weights(gradient, params, scale:float, lr:float):
+def update_weights(gradient, params, scale: float, lr: float):
     """
     Computes updated weights according to passed gradient and weight
     perturbation as learning rule
@@ -23,9 +23,10 @@ def update_weights(gradient, params, scale:float, lr:float):
     delta_W = {key: -lr / scale**2 * val for key, val in gradient.items()}
     return dictionary_add(delta_W, params)
 
+
 def sample_perturbation(sampler, params):
     """
-    Samples a perturbation dictionary with the same shape as params with the 
+    Samples a perturbation dictionary with the same shape as params with the
     sampler's distribution
 
     Parameters
@@ -45,6 +46,7 @@ def sample_perturbation(sampler, params):
         h[key] = sampler.sample(sample_shape=val.shape)
     return h
 
+
 def dictionary_add(dict1: dict, dict2: dict):
     """
     Computes the sum of two dictionaries per key.
@@ -61,6 +63,24 @@ def dictionary_add(dict1: dict, dict2: dict):
 
     """
     return {key: val + dict2[key] for key, val in dict1.items()}
+
+
+def dictionary_mult(dictionary, c):
+    """
+    Computes the multiplication of all dictionary items with a constant.
+
+    Parameters
+    ----------
+    dictionary : dict
+    c : Numeric
+
+    Returns
+    -------
+    dict
+        Original dictionary multiplied by c
+    """
+    return {key: val * c for key, val in dictionary.items()}
+
 
 def compute_gradient(forward_pass, inputs, params, sampler, method="ffd"):
     """
@@ -102,14 +122,72 @@ def compute_gradient(forward_pass, inputs, params, sampler, method="ffd"):
         # step 2: perturbed loss
         perturbed_loss = forward_pass(inputs, dictionary_add(params, h))
         # step 3: gradient = (clean loss - perturbed loss) * h
-        gradient = (clean_loss - perturbed_loss) * h
+        gradient = dictionary_mult(h, (clean_loss - perturbed_loss))
     elif method == "cfd":
         # step 1: perturbed loss
         perturbed_loss = forward_pass(inputs, dictionary_add(params, h))
         # step 2: reverse perturbed loss
-        neg_perturbed_loss = forward_pass(inputs, dictionary_add(params, -h))
+        neg_perturbed_loss = forward_pass(
+            inputs,
+            dictionary_add(params, dictionary_mult(h, -1)),
+        )
         # step 3: gradient = (clean loss - perturbed loss) * h
-        gradient = (perturbed_loss - neg_perturbed_loss) * h
+        gradient = dictionary_mult(h, (perturbed_loss - neg_perturbed_loss))
     else:
-        raise ValueError("Invalid option given. Choose between: {\"ffd\", \"cfd\"}")
+        raise ValueError('Invalid option given. Choose between: {"ffd", "cfd"}')
+    return gradient
+
+
+def compute_snn_gradient(forward_pass, inputs, y, params, sampler, method="ffd"):
+    """
+    Computes the gradient according to weight perturbation with one of the
+    following:
+        - Forward Finite Difference (ffd)
+        - Central Finite Difference (cfd)
+
+    Parameters
+    ----------
+    forward_pass : Callable (inputs, params) -> loss
+        Forward pass function
+    inputs : Array
+        Input data
+    y : Array
+        Target output
+    params : dict
+        Network weights and biases
+    sampler : Distribution
+        Sampling distribution for perturbation
+    method : str, optional
+        Method for approximating gradient. Options are: {"ffd", "cfd"}.
+        The default is "ffd".
+
+    Raises
+    ------
+    ValueError
+        If invalid method is given
+
+    Returns
+    -------
+    gradient : dict
+        Gradient estimation of the loss function wrt the weights as calculated
+        by the specified method of weight perturbation.
+
+    """
+    h = sample_perturbation(sampler, params)
+    if method == "ffd":
+        # step 1: clean loss
+        clean_loss = forward_pass(inputs, y)
+        # step 2: perturbed loss
+        perturbed_loss = forward_pass(inputs, y, h)
+        # step 3: gradient = (clean loss - perturbed loss) * h
+        gradient = dictionary_mult(h, (clean_loss - perturbed_loss))
+    elif method == "cfd":
+        # step 1: perturbed loss
+        perturbed_loss = forward_pass(inputs, y, h)
+        # step 2: reverse perturbed loss
+        neg_perturbed_loss = forward_pass(inputs, y, dictionary_mult(h, -1))
+        # step 3: gradient = (clean loss - perturbed loss) * h
+        gradient = dictionary_mult(h, (perturbed_loss - neg_perturbed_loss))
+    else:
+        raise ValueError('Invalid option given. Choose between: {"ffd", "cfd"}')
     return gradient
